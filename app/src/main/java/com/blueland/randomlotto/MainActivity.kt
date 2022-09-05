afd4b08d9934afc22dd03be5a9be1a6f84794dab
김유현 <dbgus3577@gmail.com>
2022-09-05 21:09:59 +0900

    Add dataBinding

--- app/src/main/java/com/blueland/randomlotto/MainActivity.kt
+++ app/src/main/java/com/blueland/randomlotto/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.blueland.randomlotto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.blueland.randomlotto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
